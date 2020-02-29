71ce78a44fabebddc80ec121431ad071b5c7ab18
Sharma, Varun <varun.sharma2@cdk.com>
2020-02-29 12:18:13 +0530

    Solved medium problem: linked list components

---
+++ src/main/java/LinkedListComponents.java
@@ -0,0 +1,53 @@
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Assert;
+
+public class LinkedListComponents {
+
+    public int numComponents(ListNode head, int[] G) {
+        Set<Integer> intSet = new HashSet<>();
+        for (int i : G) {
+            intSet.add(i);
+        }
+        int count = 0;
+        while (head != null) {
+            if (intSet.contains(head.val) && (head.next == null || !intSet.contains(head.next.val))) {
+                count++;
+            }
+            head = head.next;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        LinkedListComponents dd = new LinkedListComponents();
+
+        ListNode head = getTestData();
+        int num = dd.numComponents(head, new int[] { 0, 1, 3 });
+        Assert.assertEquals(2, num);
+    }
+
+    private static ListNode getTestData() {
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+        for (int i : new int[] { 1, 2, 3 }) {
+            dummy.next = new ListNode(i);
+            dummy = dummy.next;
+        }
+        return head;
+    }
+
+    static class ListNode {
+
+        int val;
+
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+
+    }
+}
