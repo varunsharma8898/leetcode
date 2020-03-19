5d84f0dab69a337f83afeb8d0f57c3fc077d5e49
Sharma, Varun <varun.sharma2@cdk.com>
2020-03-19 12:58:13 +0530

    Solved easy problem: House Robber - using top-down approach (recursion)

--- src/main/java/HouseRobber.java
+++ src/main/java/HouseRobber.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 import org.junit.Assert;
 
 public class HouseRobber {
@@ -13,9 +15,28 @@ public class HouseRobber {
         return prev1;
     }
 
+    public int robRecursively(int[] nums) {
+        int[] robbed = new int[nums.length + 1];
+        Arrays.fill(robbed, -1);
+        return robRecursively(nums, nums.length - 1, robbed);
+    }
+
+    private int robRecursively(int[] nums, int i, int[] robbed) {
+        if (i < 0) {
+            return 0;
+        }
+        if (robbed[i] >= 0) {
+            return robbed[i];
+        }
+        robbed[i] = Math.max(robRecursively(nums, i - 2, robbed) + nums[i], robRecursively(nums, i - 1, robbed));
+        return robbed[i];
+    }
+
     public static void main(String[] args) {
         HouseRobber hr = new HouseRobber();
         Assert.assertEquals(4, hr.rob(new int[] { 1, 2, 3, 1 }));
         Assert.assertEquals(12, hr.rob(new int[] { 2, 7, 9, 3, 1 }));
+        Assert.assertEquals(4, hr.robRecursively(new int[] { 1, 2, 3, 1 }));
+        Assert.assertEquals(12, hr.robRecursively(new int[] { 2, 7, 9, 3, 1 }));
     }
 }
