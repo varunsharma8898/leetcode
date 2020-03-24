28e5a199e8979118a4a6bd5e626371975212686f
Sharma, Varun <varun.sharma2@cdk.com>
2020-03-24 12:38:13 +0530

    Solved medium problem: House Robber 2 - using recursion

--- src/main/java/HouseRobber2.java
+++ src/main/java/HouseRobber2.java
@@ -27,10 +27,41 @@ public class HouseRobber2 {
         return prev1;
     }
 
+    public int robRecursively(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int len = nums.length;
+        if (len == 1) {
+            return nums[0];
+        }
+
+        int[] robFromFirst = new int[len];
+        int[] robFromSecond = new int[len];
+        Arrays.fill(robFromFirst, -1);
+        Arrays.fill(robFromSecond, -1);
+        return Math.max(robRecursively(nums, 0, len - 1, robFromFirst), robRecursively(nums, 1, len, robFromSecond));
+    }
+
+    private int robRecursively(int[] nums, int curr, int length, int[] robbed) {
+        if (curr >= length) {
+            return 0;
+        }
+        if (robbed[curr] >= 0) {
+            return robbed[curr];
+        }
+
+        robbed[curr] = Math.max(robRecursively(nums, curr + 2, length, robbed) + nums[curr], robRecursively(nums, curr + 1, length, robbed));
+        return robbed[curr];
+    }
+
     public static void main(String[] args) {
         HouseRobber2 hr = new HouseRobber2();
         Assert.assertEquals(3, hr.rob(new int[] { 2, 3, 2 }));
         Assert.assertEquals(4, hr.rob(new int[] { 1, 2, 3, 1 }));
+
+        Assert.assertEquals(3, hr.robRecursively(new int[] { 2, 3, 2 }));
+        Assert.assertEquals(4, hr.robRecursively(new int[] { 1, 2, 3, 1 }));
     }
 
 }
