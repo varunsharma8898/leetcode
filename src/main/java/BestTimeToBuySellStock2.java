4e49bc695a3afaece464000e39821eba452ccfdb
Sharma, Varun <varun.sharma2@cdk.com>
2020-03-26 10:38:13 +0530

    Solved easy problem: Best time to buy and sell a stock 2 - brute force

---
+++ src/main/java/BestTimeToBuySellStock2.java
@@ -0,0 +1,37 @@
+import org.junit.Assert;
+
+public class BestTimeToBuySellStock2 {
+
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+        int maxProfit = 0;
+        int otherMax = 0;
+        for (int i = 1; i < prices.length; i++) {
+            int currMax = prices[i] - prices[i - 1];
+            if (currMax > 0) {
+                otherMax += currMax;
+                otherMax = Math.max(currMax, otherMax);
+            } else {
+                maxProfit += otherMax;
+                otherMax = 0;
+            }
+        }
+        if (otherMax > 0) {
+            maxProfit += otherMax;
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        BestTimeToBuySellStock2 b = new BestTimeToBuySellStock2();
+        Assert.assertEquals(7, b.maxProfit(new int[] { 7, 1, 5, 3, 6, 4 }));
+        Assert.assertEquals(4, b.maxProfit(new int[] { 1, 2, 3, 4, 5 }));
+        Assert.assertEquals(0, b.maxProfit(new int[] { 7, 6, 4, 3, 1 }));
+        Assert.assertEquals(1, b.maxProfit(new int[] { 1, 2 }));
+        Assert.assertEquals(0, b.maxProfit(null));
+        Assert.assertEquals(3, b.maxProfit(new int[] { 2, 1, 4 }));
+    }
+}
