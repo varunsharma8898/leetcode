a97a5d0f9139c32347c76eddcbcc72e40c17db81
Varun Sharma <varunsharma8898@gmail.com>
2020-07-01 10:15:13 +0530

    Solved easy problem: Roman to Integer - brute force, needs refactoring

---
+++ src/main/java/RomanToInteger.java
@@ -0,0 +1,68 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+
+public class RomanToInteger {
+
+    public int romanToInt(String s) {
+        Map<Character, Integer> symbols = new HashMap<>();
+        symbols.put('I', 1);
+        symbols.put('V', 5);
+        symbols.put('X', 10);
+        symbols.put('L', 50);
+        symbols.put('C', 100);
+        symbols.put('D', 500);
+        symbols.put('M', 1000);
+
+        int returnVal = 0;
+        for (int i = 0; i < s.length(); i++) {
+            switch (s.charAt(i)) {
+                case 'I':
+                    if (i + 1 < s.length() && (s.charAt(i + 1) == 'V' || s.charAt(i + 1) == 'X' || s.charAt(i + 1) == 'M')) {
+                        returnVal += symbols.get(s.charAt(i + 1)) - symbols.get(s.charAt(i));
+                        i += 1;
+                    } else {
+                        returnVal += symbols.get(s.charAt(i));
+                    }
+                    break;
+                case 'X':
+                    if (i + 1 < s.length() && (s.charAt(i + 1) == 'L' || s.charAt(i + 1) == 'C')) {
+                        returnVal += symbols.get(s.charAt(i + 1)) - symbols.get(s.charAt(i));
+                        i += 1;
+                    } else {
+                        returnVal += symbols.get(s.charAt(i));
+                    }
+                    break;
+                case 'V':
+                case 'L':
+                case 'D':
+                case 'M':
+                    returnVal += symbols.get(s.charAt(i));
+                    break;
+                case 'C':
+                    if (i + 1 < s.length() && (s.charAt(i + 1) == 'D' || s.charAt(i + 1) == 'M')) {
+                        returnVal += symbols.get(s.charAt(i + 1)) - symbols.get(s.charAt(i));
+                        i += 1;
+                    } else {
+                        returnVal += symbols.get(s.charAt(i));
+                    }
+                    break;
+            }
+        }
+        return returnVal;
+    }
+
+    public static void main(String[] args) {
+        RomanToInteger romanToInteger = new RomanToInteger();
+        Assert.assertEquals(3, romanToInteger.romanToInt("III"));
+        Assert.assertEquals(4, romanToInteger.romanToInt("IV"));
+        Assert.assertEquals(9, romanToInteger.romanToInt("IX"));
+        Assert.assertEquals(40, romanToInteger.romanToInt("XL"));
+        Assert.assertEquals(90, romanToInteger.romanToInt("XC"));
+        Assert.assertEquals(400, romanToInteger.romanToInt("CD"));
+        Assert.assertEquals(900, romanToInteger.romanToInt("CM"));
+        Assert.assertEquals(58, romanToInteger.romanToInt("LVIII"));
+        Assert.assertEquals(1994, romanToInteger.romanToInt("MCMXCIV"));
+    }
+}
