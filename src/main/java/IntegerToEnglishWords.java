ebd71b7d79c438c5eab18245a5e6f9afc374c4a6
Varun Sharma <varunsharma8898@gmail.com>
2020-07-03 12:25:13 +0530

    Solved hard problem: Integer to English words

---
+++ src/main/java/IntegerToEnglishWords.java
@@ -0,0 +1,48 @@
+import org.junit.Assert;
+
+public class IntegerToEnglishWords {
+
+    private static final String[] SPECIALS = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+            "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+
+    private static final String[] TENS = { "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
+    private static final String[] THOUSANDS = { "", "Thousand", "Million", "Billion" };
+
+    public String numberToWords(int num) {
+
+        if (num == 0) return "Zero";
+        String res = "";
+        int index = 0;
+        while (num > 0) {
+            if (num % 1000 != 0) {
+                res = helper(num % 1000) + THOUSANDS[index] + " " + res;
+            }
+            index++;
+            num /= 1000;
+        }
+        return res.trim();
+    }
+
+    private String helper(int num) {
+        if (num == 0) {
+            return "";
+        } else if (num < 20) {
+            return SPECIALS[num] + " ";
+        } else if (num < 100) {
+            return TENS[num / 10] + " " + helper(num % 10);
+        } else {
+            return SPECIALS[num / 100] + " Hundred " + helper(num % 100);
+        }
+    }
+
+    public static void main(String[] args) {
+        IntegerToEnglishWords itew = new IntegerToEnglishWords();
+        Assert.assertEquals("One Hundred Twenty Three", itew.numberToWords(123));
+        Assert.assertEquals("Twelve Thousand Three Hundred Forty Five", itew.numberToWords(12345));
+        Assert.assertEquals("One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven", itew.numberToWords(1234567));
+        Assert.assertEquals("One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One",
+                itew.numberToWords(1234567891));
+    }
+
+}
