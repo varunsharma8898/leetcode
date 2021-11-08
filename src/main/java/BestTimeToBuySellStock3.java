42938d6dc3a4b299464a26f41791d60885debd8b
Varun Sharma <varun.sharma@sincrodigital.com>
2021-11-08 11:40:13 +0530

    Solved problem - best time to buy sell stock 3

--- src/main/java/BestTimeToBuySellStock3.java
+++ src/main/java/BestTimeToBuySellStock3.java
@@ -5,13 +5,14 @@ import org.junit.Assert;
 
 public class BestTimeToBuySellStock3 {
 
-    public int maxProfit(int[] prices) {
+    public int maxProfit_(int[] prices) {
         if (prices == null || prices.length < 2) {
             return 0;
         }
 
-        int oneBuyOneSell = 0, twoBuyTwoSell = 0;
         int oneBuyPrice = Integer.MAX_VALUE, twoBuyPrice = Integer.MAX_VALUE;
+        int oneBuyOneSell = 0, twoBuyTwoSell = 0;
+
         for (int price : prices) {
             oneBuyPrice = Math.min(oneBuyPrice, price);
             oneBuyOneSell = Math.max(oneBuyOneSell, price - oneBuyPrice);
@@ -19,6 +20,21 @@ public class BestTimeToBuySellStock3 {
             twoBuyTwoSell = Math.max(twoBuyTwoSell, price - twoBuyPrice);
         }
         return twoBuyTwoSell;
+    }
+
+
+    private int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 2) return 0;
+
+        int onebuy = Integer.MAX_VALUE, twobuy = Integer.MAX_VALUE;
+        int onesell = 0, twosell = 0;
+        for (int price : prices) {
+            onebuy = Math.min(onebuy, price);
+            onesell = Math.max(onesell, price - onebuy);
+            twobuy = Math.min(twobuy, price - onesell);
+            twosell = Math.max(twosell, price - twobuy);
+        }
+        return twosell;
     }
 
     public static void main(String[] args) {
